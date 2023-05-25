0c3c3331f2010b8e62c1d17cfd86ce41343692c4
Dmitriy <velkov55@gmail.com>
2023-05-25 07:26:54 +0300

    added files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,30 @@
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+import user from 'json/user.json';
+import data from 'json/data.json';
+import friends from 'json/friends.json';
+import transactions from 'json/transactions.json';
+import './App.styled';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Statistics title="Upload stats" stats={data} />
+        <FriendList friends={friends} />;
+        <TransactionHistory items={transactions} />;
+      </header>
+    </div>
+  );
+}
+
+export default App;
